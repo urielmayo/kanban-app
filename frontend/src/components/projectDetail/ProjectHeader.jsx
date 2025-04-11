9121e6a4fc30360d71c70b3ab1cc7b208c548a38
urielmayo <urimayo00@gmail.com>
2025-04-11 13:04:53 -0300

    add edit and delete button to the project

--- frontend/src/components/projectDetail/ProjectHeader.jsx
+++ frontend/src/components/projectDetail/ProjectHeader.jsx
@@ -1,10 +1,20 @@
-import { Box, Typography, Tooltip, IconButton } from "@mui/material";
+import { Box, Typography, Tooltip, IconButton, Button } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
-import { useNavigate, useParams } from "react-router-dom";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useNavigate, useParams } from "react-router";
+import { useMutation } from "@tanstack/react-query";
+import { deleteProject } from "../../utils/http";
 
 function ProjectHeader({ name, description }) {
   const navigate = useNavigate();
   const { id } = useParams();
+
+  const mutation = useMutation({
+    mutationFn: deleteProject,
+    onSuccess: () => navigate(".."),
+  });
+
   return (
     <Box sx={{ display: "flex", alignItems: "center", gap: 1, mb: 2 }}>
       <Typography variant="h4">{name}</Typography>
@@ -15,20 +25,21 @@ function ProjectHeader({ name, description }) {
           </IconButton>
         </Tooltip>
       )}
-      <button
+      <Button
         onClick={() => navigate(`/projects/${id}/edit`)} // Navegar a la página de edición
-        style={{
-          marginLeft: "auto",
-          padding: "8px 16px",
-          backgroundColor: "#2563eb",
-          color: "#fff",
-          border: "none",
-          borderRadius: "4px",
-          cursor: "pointer",
-        }}
+        variant="contained"
+        startIcon={<EditIcon />}
       >
-        Editar Proyecto
-      </button>
+        Edit
+      </Button>
+      <Button
+        onClick={() => mutation.mutate({ id })} // Navegar a la página de edición
+        variant="contained"
+        color="error"
+        startIcon={<DeleteIcon />}
+      >
+        Delete
+      </Button>
     </Box>
   );
 }
